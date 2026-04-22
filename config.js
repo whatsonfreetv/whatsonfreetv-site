8360759e39c2e9a1402f5f3b0d3704f7687da343
Elvin Fernandez <elvinfernandez@Elvins-MacBook-Air.local>
2026-04-22 13:49:07 -0700

    Fix API URL to use Cloudflare Pages function

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Config for WhatsOnFreeTV site
 const CONFIG = {
-  API_URL: '/.netlify/functions/airtable',
+  API_URL: '/api/airtable',
   OMDB_API_KEY: '846832d1'  // https://www.omdbapi.com/apikey.aspx
 };
